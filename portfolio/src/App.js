0ee7e78bab0196e7708382a43fa97b77388ca23a
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-30 00:08:23 +0200

    Linking the home component and bootstrap

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Home from './pages/Home';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.js"
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={Home}></Route>
+          <Route path='/' element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
