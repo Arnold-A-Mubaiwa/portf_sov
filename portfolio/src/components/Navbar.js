9a1e8f371f804d8cc92332f4af1f70f9d63e8d76
arnold0a-mubaiwa <arnoldmubaiwa99@gmail.com>
2022-10-30 13:41:49 +0200

    home layout and css

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,24 +1,46 @@
 import React from "react";
-
+import wind from "../assets/wind.png"
 function Navbar() {
   return (
     <div>
-      <nav className="navbar text-light navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div className="container">
-                <a className="navbar-brand" href="#page-top">Arnold Mubaiwa</a>
-                <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i className="fas fa-bars"></i>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item "><a className="nav-link  rounded" href="#portfolio">Portfolio</a></li>
-                        <li className="nav-item "><a className="nav-link  rounded" href="#about">About</a></li>
-                        <li className="nav-item "><a className="nav-link  rounded" href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>    
+      <nav className="navbar  navbar-expand-lg  text-uppercase fixed-top nv-color">
+        <div className="container">
+          <a className="navbar-brand" href="#page-top">
+           Arnold Mubaiwa 
+          </a>
+          <button
+            className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            Menu
+            <i className="fas fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item ">
+                <a className="nav-link  rounded" href="#portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link  rounded" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item ">
+                <a className="nav-link  rounded" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa'
-
+import { FaGithub,  FaLinkedin } from 'react-icons/fa'
+import wind from "../assets/wind.png"
 function Home() {
   return (
+    <div>
     <div className="mt-5 pt-5 d-flex h-100 text-center text-white bg-dark ">
       <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
         <main className="px-3">
@@ -14,15 +15,51 @@ function Home() {
           <h1>Anotida Arnold Mubaiwa</h1>
           <p className="lead">
             <h3>Graduate Programme</h3>
-            I am a Bachelors Of Sciences in Information Technology graduate living in Cape Town. I graduated inthe year 2021 from Richfield.
-            Below are my social media accounts where you can view my work and achievements.
-          </p>
-          <div class="social-links mt-3 text-center">
+              </p>
+          <div className="social-links mt-3 text-center">
           <a href="https://github.com/Arnold-A-Mubaiwa" className="p-2"><FaGithub /></a>
           <a href="https://www.linkedin.com/in/arnold-anotida-mubaiwa-75aa1816a/" className="p-2"><FaLinkedin /></a>
           </div>
         </main>
       </div>
+</div>
+
+<section  className=" p-5 txt-sec">
+      <div className="container">
+
+        <div className="section-title">
+          <h2>About</h2>
+          <p>I am a Bachelors Of Sciences in Information Technology graduate living in Cape Town. I graduated in the year 2021 from Richfield.
+            Below are my social media accounts where you can view my work and achievements.
+        </p>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4 " data-aos="fade-right">
+            <img  src={wind}
+ className="img-fluid img-n" alt=""/>
+          </div>
+          <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Programming Languages</h3>
+            <p className="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <div className="row">
+              <div className="col-lg-6">
+                <ul>
+                  <li><i className="bi bi-chevron-right"></i> <strong>language</strong> <span>1 May 1995</span></li>
+                  </ul>
+              </div>
+
+            </div>
+            <p>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
     </div>
   );
 }
